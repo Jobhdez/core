c2dd0c5345bc3fb4309e6451d9bca01132404ebc
Job Hernandez <hj93@protonmail.com>
2022-11-08 09:34:29 -0800

    add new functionality.

--- src/passes/select-instructions.lisp
+++ src/passes/select-instructions.lisp
@@ -1,5 +1,8 @@
 (in-package #:zetta)
 
+(defparameter *registers* (make-hash-table :test 'equal))
+(defparameter *expressions* (make-hash-table))
+
 (defstruct immediate
   int)
 
@@ -20,37 +23,93 @@
   label)
 
 (defun select-instructions (ast)
-  (flatten (mapcar (lambda (n) (select-instrs n)) (cdr ast))))
+  (flatten (mapcar (lambda (n) (select-instrs n)) ast)))
+
+
 
 (defun select-instrs (node)
   (match node
 	 ((py-assignment :name n
 			 :exp e1)
 	  (if (atomic-sum-p e1)
-	      (let ((tmp-var (atomic-sum-rexp e1)))
+	      (let* ((tmp-var (atomic-sum-rexp e1))
+		    (hash-values (hash-table-values *expressions*))
+		    (hash-keys (hash-table-keys *expressions*))
+		    (last-key (car (last hash-keys))))
+		     
 		(list (make-instruction :name "movq"
 				        :arg1 (make-immediate :int (py-constant-num
 							    (atomic-sum-lexp e1)))
 				        :arg2 n)
-		      (make-instruction :name "addq"
-				        :arg1 tmp-var
-				        :arg2 n)))))
+		      (if (equalp 'py-neg-num last-key)
+			  (make-instruction :name "addq"
+				        :arg1 n
+				        :arg2 (gethash "%rax" *registers*))
+			  (make-instruction :name "addq"
+					    :arg1 tmp-var
+					    :arg2 n))))))
+				
+						 
+						 
+		        
 	 ((atomic-assignment :temp-var tmp
 			     :n n)
-	  (if (atomic-sum-p n)
+	  (cond ((atomic-sum-p n)
 	      (let ((vari (atomic-sum-lexp n))
 		    (rexp (py-constant-num (py-sum-rexp (atomic-sum-rexp n)))))
 		(list (make-instruction :name "movq"
-				 :arg1 vari
+					:arg1 (if (equalp 'py-neg-num (gethash 'py-neg-num *expressions*))
+						  "%rax"
+						  vari)
 				 :arg2 tmp)
 		      (make-instruction :name "addq"
 					:arg1 (make-immediate :int rexp)
-					:arg2 tmp)))))
+					:arg2 tmp))))
+		((py-neg-num-p n)
+		 (let* ((num (py-constant-num (py-neg-num-num n)))
+			(tmp-var tmp))
+		   (setf (gethash "%rax" *registers*) "%rax")
+		   (setf (gethash 'py-neg-num *expressions*) 'py-neg-num)
+		   (list
+		    (make-instruction :name "movq"
+				      :arg1 num
+				      :arg2 tmp-var)
+		    (make-instruction :name "negq"
+				      :arg1 tmp-var
+				      :arg2 'no-sec-arg)
+		    (make-instruction :name "movq"
+				      :arg1 tmp-var
+				      :arg2 (gethash "%rax" *registers*)))))))
 	 ((py-print :exp e1)
-	  (list (make-instruction :name "movq"
-				  :arg1 e1
+	  (let* ((hash-values (hash-table-values *expressions*))
+	      	 (hash-keys (hash-table-keys *expressions*))
+		 (last-key (car (last hash-keys))))
+	    (list (if (equalp 'py-neg-num last-key)
+		      (make-instruction :name "movq"
+				  :arg1 "%rax"
 				  :arg2 "%rdi")
-		(make-callq :label "print_int")))))
+		      (make-instruction :name "movq"
+					:arg1 e1
+					:arg2 "%rdi"))
+		
+		(make-callq :label "print_int"))))
+	 ((atomic-sum :lexp e1 :rexp e2)
+	  (cond ((py-constant-p e1)
+		 
+		 (let* ((hash-values (hash-table-values *registers*))
+		       (hash-keys (hash-table-values *registers*))
+		       (last-key (car (last hash-keys)))
+		       (reg (gethash last-key *registers*)))
+		   (remhash last-key *registers*)
+		   (list (make-instruction :name "addq"
+					   :arg1 (py-constant-num e1)
+					   :arg2 reg)
+			 (make-instruction :name "retq"
+					   :arg1 'no-arg
+					   :arg2 'no-arg))))
+		(t (error "E1 isnt a constant."))))))
+		
+		   
 				  
 			   
 
