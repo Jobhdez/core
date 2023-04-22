81465207ef894b654828af11745b4e1160e5ae83
Job <hj93@protonmail.com>
2023-04-22 15:58:09 -0700

    fix hash table bug.

--- src/passes/assign-homes.lisp
+++ src/passes/assign-homes.lisp
@@ -19,7 +19,7 @@
 	  (make-callq :label la))))
 
 (defun assign-homes (instructions)
-  (let ((*varnames* (make-hash-table :test 'equal))
+  (let ((*varnames* (make-hash-table :test 'equalp))
 	(*variable* 0))
     (labels ((assignh (instr)
 	       (match instr
@@ -54,13 +54,13 @@
 			   (if (gethash a1 *varnames*)
 			       (make-instruction :name name
 				                 :arg1 (gethash a1 *varnames*)
-				                 :arg2 a2)))
-			      ;; (progn (setf *variable* (+ *variable* 8))
-		                     ;; (setf (gethash a1 *varnames*)
-		                            ;;(concatenate 'string "-" (write-to-string *variable*) "(%rbp)"))
-			              ;(make-instruction :name name
-				                        ;:arg1 (gethash a1 *varnames*)
-				                        ;:arg2 a2))))
+				                 :arg2 a2)
+			      (progn (setf *variable* (+ *variable* 8))
+		                     (setf (gethash a1 *varnames*)
+		                           (concatenate 'string "-" (write-to-string *variable*) "(%rbp)"))
+			             (make-instruction :name name
+				                       :arg1 (gethash a1 *varnames*)
+				                       :arg2 a2))))
 			   ((and (stringp a1)
 		                 (atomic-var-p a2))
 			    (if (gethash a2 *varnames*)
@@ -135,6 +135,8 @@
 					                 :arg1 a1
 					                 :arg2 (gethash a2 *varnames*)))))
 			   (t (error "Not valid Instruction."))))
+		 ((block-py :name name)
+		  instr)
 		 ((callq :label lbl)
 		  instr))))
 	     (flatten (mapcar (lambda (instr) (assignh  instr))  instructions)))))		     
@@ -144,11 +146,25 @@
   (labels ((clean-select-pass (instr)
 	     (match instr
 		    ((instruction :name name :arg1 a1 :arg2 a2)
-		     (if (and (atomic-var-p a1) (py-var-p (atomic-var-name a1)))
-			 (make-instruction :name name
-					   :arg1 (make-atomic-var :name (py-var-name (atomic-var-name a1)))
-					   :arg2 a2)
-			 instr)))))
+		     (cond ((and (atomic-var-p a1) (py-var-p (atomic-var-name a1)))
+			    (make-instruction :name name
+					      :arg1 (make-atomic-var :name (py-var-name (atomic-var-name a1)))
+					      :arg2 a2))
+			   ((and (py-var-p a1) (not (py-var-p a2)))
+			    (make-instruction :name name
+					      :arg1 (make-atomic-var :name (py-var-name a1))
+					      :arg2 a2))
+			   ((py-constant-p a1)
+			    (make-instruction :name name
+					      :arg1 (make-immediate :int (py-constant-num a1))
+					      :arg2 a2))
+			   (t
+			    instr)))
+		    ((block-py :name name)
+		     instr)
+		    
+		    ((callq :label label)
+		     instr))))
     (flatten (mapcar (lambda (instr) (clean-select-pass instr)) ast))))
 
 (defun without-last (lst)
