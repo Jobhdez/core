73ac71b17f3383bb336d08630126f94fe1da4435
Job <hj93@protonmail.com>
2023-04-27 12:15:55 -0700

    fix indentation.

--- src/passes/expose-allocation.lisp
+++ src/passes/expose-allocation.lisp
@@ -12,22 +12,22 @@
 
 (defun expose-allocation (tup)
   (match tup
-	 ((tuple :int elements)
-	  (let ((temp-names (mapcar (lambda (element) (make-temp-variable element)) elements)))
-	    (make-begin :statements (flatten
-				     (list
-				      temp-names
-					
-					(make-py-if :exp (make-py-cmp :lexp (make-py-sum :lexp (make-global-value :arg1 'free_ptr)
-											 :rexp (make-py-constant :num (* (length elements) 4)))
-								      :cmp  "<"
-								      :rexp (make-global-value :arg1 'fromspace_end))
-						    :if-statement (make-py-constant :num 0)
-						    :else-statement (make-collect :bytes (* (length elements) 4)))
-					(make-py-assignment :name 'v
-							    :exp (make-allocate :len (length elements)
-										:type (make-*type* :type 'int)))
-					(mapcar (lambda (e) (make-array-variable e)) (mapcar (lambda (element) (py-assignment-name element)) temp-names)))))))))
+    ((tuple :int elements)
+     (let ((temp-names (mapcar (lambda (element) (make-temp-variable element)) elements)))
+       (make-begin :statements (flatten
+				(list
+				 temp-names
+				 
+				 (make-py-if :exp (make-py-cmp :lexp (make-py-sum :lexp (make-global-value :arg1 'free_ptr)
+										  :rexp (make-py-constant :num (* (length elements) 4)))
+							       :cmp  "<"
+							       :rexp (make-global-value :arg1 'fromspace_end))
+					     :if-statement (make-py-constant :num 0)
+					     :else-statement (make-collect :bytes (* (length elements) 4)))
+				 (make-py-assignment :name 'v
+						     :exp (make-allocate :len (length elements)
+									 :type (make-*type* :type 'int)))
+				 (mapcar (lambda (e) (make-array-variable e)) (mapcar (lambda (element) (py-assignment-name element)) temp-names)))))))))
 
 (defvar temp -1)
 (defun make-temp-variable (element)
