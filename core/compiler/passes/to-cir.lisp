9fafa3b5bad66c98c231a77d90781d3516b3c3b0
Job Hernandez <hj93@protonmail.com>
2024-07-22 16:11:56 -0700

    add support of if atomics

--- core/compiler/passes/to-cir.lisp
+++ core/compiler/passes/to-cir.lisp
@@ -1,7 +1,13 @@
 (in-package :core)
 
 (defstruct if-goto-loop
-  cnd body)
+  cnd body blocks)
+
+(defstruct if-goto
+  cnd thn els blocks)
+
+(defstruct goto
+  block)
 
 (defun to-cir (anf)
   (let ((blocks (make-hash-table :test 'equalp))
@@ -13,11 +19,19 @@
 		    (progn
 		      (setf (gethash loop-block blocks) (toc tblk))
 		      (setf (gethash loop-block blocks) (mapcar (lambda (e) (toc e)) blk))
-		      (let ((goto* (make-if-goto-loop :cnd (toc tblk) :body (concatenate 'string "goto " loop-block))))
+		      (let ((goto* (make-if-goto-loop :cnd (toc tblk) :body (make-goto :block loop-block) :blocks blocks)))
 			(flatten
 			 (list (mapcar (lambda (e) (toc e)) pblk)
-			       goto*
-			       blocks))))))
+			       goto*))))))
+		 ((if-atomic :thn thn :els els :cnd cnd)
+		  (let ((blk-thn (concatenate 'string "block_" (write-to-string (setf counter (+ counter 1)))))
+			(blk-els (concatenate 'string "block_" (write-to-string (setf counter (+ counter 2))))))
+		    (progn
+		      (setf (gethash blk-thn blocks) (if (listp thn) (mapcar (lambda (e) (toc e)) thn) thn))
+		      (setf (gethash blk-els blocks) (if (listp els) (mapcar (lambda (e) (toc e)) els) els))
+		      (list (make-if-goto :cnd (toc cnd) :thn (make-goto :block blk-thn) :els (make-goto :block blk-els) :blocks blocks)))))
+			
+		 
 		 (_ instr))))
       (mapcar (lambda (instr) (toc instr)) anf))))
 		      
